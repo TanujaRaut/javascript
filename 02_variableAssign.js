b4929123d4f321c28c6a2c8fcbe131c649bf1872
TanujaRaut <tanuja2001raut@gmail.com>
2023-12-26 20:56:40 +0530

    Variable JS
    Variable JS

---
+++ 02_variableAssign.js
@@ -0,0 +1,39 @@
+var myFullName;
+console.log(myFullName);
+
+myName = "Tanuja Raut"
+console.log("Full Name: ",myName);
+
+myName = "Tanuja Ramkrushna Raut";
+console.log("Full Name: ",myName);
+
+var myLove = "I love only JS";
+console.log(myLove);
+
+myName = "Tanuja";
+console.log("First Name: ",myName);
+
+myName = "Raut";
+console.log("Last Name: ",myName);
+
+var age = "22"; 
+console.log("Age:", age);
+
+var Address;
+Address = "Pune";
+console.log("Address: ", Address)
+
+var pincode;
+pincode = "411052";
+console.log("pinCode: ", pincode);
+
+var Gender;
+Gender = "Female";
+console.log("Gender: ", Gender);
+
+var Married; 
+Married = "No";
+console.log("is Married: ", Married);
+
+
+
