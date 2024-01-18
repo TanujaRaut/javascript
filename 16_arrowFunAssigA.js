a4c9979368d420d7b1d65ea5b9c8488f748497ba
TanujaRaut <tanuja2001raut@gmail.com>
2024-01-18 16:22:48 +0530

    JS
    JS

---
+++ 16_arrowFunAssigA.js
@@ -0,0 +1,30 @@
+console.log("=======================Step 1==================================");
+
+const show = () => {
+    console.log("Good Morning, Today is Thursday");
+}
+show()
+
+console.log("=======================Step 2==================================");
+
+const multiply = (num1,num2,num3) => {
+    console.log(`The Multiplication is : ${num1*num2*num3} `);
+}
+multiply(5,5,2);
+multiply(10,4,1)
+
+console.log("=======================Step 3==================================");
+
+const add = (num1,num2,num3,num4,num5) => {
+console.log(`The addition is : ${num1+num2+num3+num4+num5}`);
+}
+add(100,100,200,349,756)
+add("I am","learning","ES6",'features',"in depth")
+
+
+
+
+
+
+
+
