96cc009dd19a0d7b833a71acd48b38f0b2863d25
TanujaRaut <tanuja2001raut@gmail.com>
2024-01-24 21:35:26 +0530

    DOM
    DOM

---
+++ DOM/14_main.js
@@ -0,0 +1,6 @@
+console.log(`************* Changing Heading color ************* `);
+const headingChange = document.querySelector('#myPrfl');
+headingChange.style.color="cadetblue";
+console.log(`************* Changing Project Description ************* `);
+let projectDis = document.querySelector("#prjctDscrption");
+projectDis.innerHTML='Project Description : This project is base on HTML and CSS';
